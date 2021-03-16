69360d0bea4c4af3b57cfcbfec84129507254618
kkalugerov <kkalugerov@gmail.com>
2021-03-16 15:03:58 +0200

    Use go run instead go test

--- job-inputs/task_run_tests.sh
+++ job-inputs/task_run_tests.sh
@@ -8,4 +8,4 @@ set -e
 export GOPATH=$(pwd)/gopath:$(pwd)/gopath/src/github.com/cloudfoundry-community/simple-go-web-app/Godeps/_workspace
 cd gopath/src/github.com/cloudfoundry-community/simple-go-web-app/
 
-go test ./...
+go run main.go
